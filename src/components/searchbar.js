685ac1a0543b25cda7e396ec5c8fa91c2d34d39a
Ryan Ferguson <fergusman@gmail.com>
2017-06-16 16:03:10 -0400

    push check

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -5,7 +5,7 @@ import { Navbar } from 'react-materialize'
 export default (props) => {
   return (
     <Navbar className="blue-grey darken-4 inverse">
-      <h4>NiteStand</h4>
+      <h4>NiteStand</h4> 
     </Navbar>
 
   )
