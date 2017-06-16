4b27c814b9d05216b1f69043a7d0f17a787ff7b4
iwpeifer <iwpeifer@gmail.com>
2017-06-16 13:17:48 -0400

    making ugly cards

--- src/components/articleCard.js
+++ src/components/articleCard.js
@@ -1,13 +1,36 @@
 import React from 'react'
-import { Col, Card } from 'react-materialize'
+import { Col, Button } from 'react-materialize'
 // import { Link } from 'react-router-dom'
+
+// export default (props) => {
+//   return (
+//     <Col m={4} s={12}>
+//       <Card className='container-shit blue-grey lighten-1' textClassName='white-text'
+//       title={props.article.headline} actions={[<a href={props.article.web_url}
+//         target="_blank" rel="noopener noreferrer">Go to Story</a>,
+//         <a href='#' className="lime-text hoverable" target="_blank"
+//         rel="noopener noreferrer">Add to Reading List</a>]}>
+//         {props.article.lead_paragraph.substring(0, 140) + "..."}
+//       </Card>
+//     </Col>
+//
+//   )
+// }
 
 export default (props) => {
   return (
-    <Col m={6} s={12}>
-      <Card className='blue-grey darken-1' textClassName='white-text' title={props.article.headline} actions={[<a href={props.article.web_url} className="blue-text hoverable" target="_blank" rel="noopener noreferrer">Go to Story</a>, <a href='#' className="lime-text right hoverable" target="_blank" rel="noopener noreferrer">Add to Reading List</a>]}> {props.article.lead_paragraph.substring(0, 140) + "..."}
-      </Card>
+    <Col m={4} s={12}>
+      <div className="card">
+        <span className="card-top">
+          <a href={props.article.web_url}  target="_blank" rel="noopener noreferrer"><h5 className="card-title">{props.article.headline}</h5></a>
+        </span>
+        <div className="card-contents">
+          <p>{props.article.lead_paragraph}</p>
+        </div>
+        <div className="card-bottom">
+          Published: {props.article.pub_date} <Button className="orange" waves='light'>add</Button>
+        </div>
+      </div>
     </Col>
-
   )
 }
---
+++ src/components/searchbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navbar } from 'react-materialize'
+// import { Link } from 'react-router-dom'
+
+export default (props) => {
+  return (
+    <Navbar className="blue-grey darken-4 inverse">
+      <h4>NiteStand</h4>
+    </Navbar>
+
+  )
+}
